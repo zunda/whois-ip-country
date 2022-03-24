bf129e9a4f841b2affdec42d4fc256a09c3bb643
zunda <zundan@gmail.com>
2022-03-23 19:11:03 -1000

    Better pick up CIDRs

--- country.rb
+++ country.rb
@@ -10,7 +10,7 @@ ARGF.each do |text|
   ip = text.scan(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/).first
   if ip
     r = whois.lookup(ip).content
-    cidr = r.scan(/^(?:route|cidr|inetnum):.*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d+)/i).flatten.first
+    cidr = r.scan(/^(?:route|cidr|inetnum):.*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d+)/i).flatten.first
     country = r.scan(/^country:\s*(.+)$/i).flatten.first
     puts "#{ip}\t#{cidr}\t#{country}"
     sleep 1
